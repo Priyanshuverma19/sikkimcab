00d58d5fa034875d92b0ba8a08c75757233555b4
priyanshu verma <priyanshu.vermaa0@gmail.com>
2025-04-25 18:46:53 +0530

    add testemonial

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <HeroSection />
       <ServicesSection />
       {/* <AboutSection /> */}
-      {/* <TestimonialsSection /> */}
+      <TestimonialsSection />
       {/* <ContactSection /> */}
     </div>
   );
